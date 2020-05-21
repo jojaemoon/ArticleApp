2250936360fef51c690a7d1e6d4cd692eaf4f6b3
jaemoon jo <fall_tree@daum.net>
2020-05-21 20:54:01 +0900

    두번째

---
+++ ArticleApp.Models.Tests/Article.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArticleApp.Models.Tests
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+    }
+}
---
+++ ArticleApp.SqlServer/dbo/Tables/Articles.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Articles]
+(
+	[Id] INT NOT NULL PRIMARY Key Identity(1, 1),
+	[Title] NVarChar(255) Not Null,
+)
+Go
